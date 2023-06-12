1a27ddb862a0e033f8ddc29f202c47a70b6df31b
capacible <cpapacible@up.edu.ph>
2023-06-12 13:53:08 +0800

    transform

--- scripts/index.js
+++ scripts/index.js
@@ -9,16 +9,18 @@ function draw(gl)
     // we use allrain in draw
     createRain(allRain, 1, 1, (1,1,1,1));
 
-    // setting the data for the buffer
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(allRain), gl.STATIC_DRAW);
-
-    // clear the screen
-    gl.clearColor(-1.0, -1.0, -1.0 , 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
     console.log("cleared screen");
-    console.log(allRain);
     
     gl.drawArrays(gl.LINE, 0, allRain.length);
 
+}
+
+
+function translate(translate, transformMatrix)
+{
+    // we basically remember the old transform matrix and add that with our new translation value.
+    for(let i=0;i<transformMatrix.length;i++)
+    {
+        transformMatrix[i] = transformMatrix[i] + translate[i];
+    }
 }
